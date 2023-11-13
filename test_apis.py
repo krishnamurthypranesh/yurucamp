1d03869d7744fc364116616d721760a6ebf3528e
krishnamurthypranesh <pk96ishere@gmail.com>
2023-11-13 12:52:20 +0000

    Update test script
    Remove print statements

--- test_apis.py
+++ test_apis.py
@@ -1,6 +1,6 @@
 import requests
 
-username = "pranesh"
+username = "test@test.com"
 password = "pass1234"
 
 headers = {
@@ -8,23 +8,34 @@ headers = {
 }
 
 res = requests.post(
-    "http://localhost:8000/v1/user-sessions/",
-    json={"username": "pranesh", "password": "pass1234"},
+    "http://localhost:8000/v1/authn/user-sessions/",
+    json={"username": username, "password": password},
     headers=headers,
 )
 print(res)
+token = res.json()["token"]
 
 
-# res = requests.get("http://localhost:8000/v1/trips/locations/", headers=headers)
+headers["Authorization"] = f"Bearer {token}"
+res = requests.get("http://localhost:8000/v1/trips/locations/", headers=headers)
+print(res)
 
-# print(res.json())
 
-# res = requests.get("http://localhost:8000/v1/trips/weather-check/", headers=headers, params={
-#                     "latitude": "19.0760",
-#                     "longitude": "72.8777",
-#                     "trip_date": "2023-11-10",
-# })
-# print(res.json())
+res = requests.get(
+    "http://localhost:8000/v1/trips/weather-check/",
+    headers=headers,
+    params={
+        "latitude": "19.0760",
+        "longitude": "72.8777",
+        "trip_date": "2023-11-10",
+    },
+)
+print(res.json())
 
-# res = requests.post("http://localhost:8000/v1/trips/", headers=headers, json={"is_draft": False, "itinerary": {}})
-# print(res.status_code, res.json())
+res = requests.post(
+    "http://localhost:8000/v1/trips/",
+    headers=headers,
+    json={"is_draft": False, "itinerary": {}},
+)
+
+print(res.status_code, res.json())
